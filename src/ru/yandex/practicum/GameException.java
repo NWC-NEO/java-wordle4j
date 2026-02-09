52350ea044c15082fb9e1f3f2c9bfcdcdaa3774c
Артем Буренин <ironman1822@mail.ru>
2026-02-09 19:56:53 +0400

    style: fix code formatting according to checkstyle

--- src/ru/yandex/practicum/GameException.java
+++ src/ru/yandex/practicum/GameException.java
@@ -1,6 +1,12 @@
 package ru.yandex.practicum;
 
 public class GameException extends Exception {
-    public GameException(String msg) { super(msg); }
-    public GameException(String msg, Throwable t) { super(msg, t); }
+
+    public GameException(String msg) {
+        super(msg);
+    }
+
+    public GameException(String msg, Throwable t) {
+        super(msg, t);
+    }
 }
